3698b24c5a2e30d60e906ffa74978c8c897f999c
Brighter Myanmar <brightermyanmar@gmail.com>
2022-05-10 15:15:49 +0630

    Almost Donny

--- utils/validator.js
+++ utils/validator.js
@@ -33,11 +33,12 @@ module.exports = {
          let token = req.headers.authorization.split(" ")[1];
          if (token) {
             try {
+
                // let decoded = jwt.decode(token,process.env.SECRET_KEY);
                // console.log(Date.now());
-               // console.log("Decoded",decoded);
-               // if(Date.now() >= decoded.exp * 1000){
-               //    console.log("Token Expired");
+               // console.log(decoded);
+               // if(Date.now()  >= decoded.exp * 1000){
+               //    console.log("Expired");
                // }else{
                //    console.log("Token is active");
                // }
